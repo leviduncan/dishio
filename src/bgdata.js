8b32ead91025c40ed0b1d553f5f31327851744a3
leviduncan <darrin@darrinduncan.com>
2024-10-19 21:10:01 -0400

    JSX update

---
+++ src/bgdata.js
@@ -0,0 +1,12 @@
+const BGLIST = [
+    {
+        name: 'hero1',
+        url: './assets/hero1.jpg'
+    },
+    {
+        name: 'hero2',
+        url: './assets/hero2.jpg'
+    },
+]
+
+export default BGLIST
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,36 +1,12 @@
-import bg from '../assets/hero1.jpg'
+import FeaturedRecipes from './FeaturedRecipes'
 
 const Home = () => {
     
     return (
-        <section className="hero-container">
-                <picture>
-                    <source
-                        media="(max-width: 767px)"
-                        srcSet={bg}
-                        type="image/jpeg" />
-                    <source
-                        media="(min-width: 768px)"
-                        srcSet={bg}
-                        type="image/jpeg" />
-                    <img
-                        src="desktop-image.jpg"
-                        alt="Your image description"
-                        loading="lazy" />
-                </picture>
-
-            <div className="px-4 py-5 my-5 text-center hero-content">
-                <h1 className="display-5 fw-bold">Centered hero</h1>
-                <div className="col-lg-6 mx-auto">
-                    <p className="lead mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world's most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
-                    <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
-                        <button type="button" className="btn btn-primary btn-lg px-4 gap-3">Primary button</button>
-                        <button type="button" className="btn btn-outline-secondary btn-lg px-4">Secondary</button>
-                    </div>
-                </div>
-            </div>
-
-        </section>
+        <>
+        <Home />
+       <FeaturedRecipes />
+        </>
     )
 }
 
