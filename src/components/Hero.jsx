f6f77e6f943878ff7efad5ebe4b03132730912e9
darrin@darrinduncan.com <darrin@darrinduncan.com>
2024-10-28 23:08:29 -0400

    Add Hero images

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import { useContext, useState, useEffect } from 'react';
 import { RecipeContext } from '../context/recipeContext';
 import Loading from './Loading'
 import SearchBar from './SearchBar';
-import heroes from '../data/bgdata'
+import heroes from '../data/hero'
 
 const chooseRandomHero = (heroes) => {
   const randomIndex = Math.floor(Math.random() * heroes.length);
--- src/data/hero.js
+++ src/data/hero.js
@@ -1,19 +1,19 @@
 const heroes = [
     {
       name: 'hero1',
-      url: '../../src/assets/hero1.jpg'
+      url: '../../src/assets/hero1.webp'
     },
     {
       name: 'hero2',
-      url: '../../src/assets/hero2.jpg'
+      url: '../../src/assets/hero2.webp'
     },
     {
       name: 'hero3',
-      url: '../../src/assets/hero3.jpg'
+      url: '../../src/assets/hero3.webp'
     },
     {
       name: 'hero4',
-      url: '../../src/assets/hero4.jpg'
+      url: '../../src/assets/hero4.webp'
     }
   ]
   
