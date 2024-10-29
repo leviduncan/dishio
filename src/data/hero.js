8a9e80a7419820bc17674975812c0dc2f602e2f1
darrin@darrinduncan.com <darrin@darrinduncan.com>
2024-10-28 23:36:50 -0400

    Hero images from data file

--- src/data/hero.js
+++ src/data/hero.js
@@ -1,19 +1,24 @@
+import hero1 from '../../src/assets/hero1.webp'
+import hero2 from '../../src/assets/hero2.webp'
+import hero3 from '../../src/assets/hero3.webp'
+import hero4 from '../../src/assets/hero4.webp'
+
 const heroes = [
     {
       name: 'hero1',
-      url: '../../src/assets/hero1.webp'
+      url: hero1
     },
     {
       name: 'hero2',
-      url: '../../src/assets/hero2.webp'
+      url: hero2
     },
     {
       name: 'hero3',
-      url: '../../src/assets/hero3.webp'
+      url: hero3
     },
     {
       name: 'hero4',
-      url: '../../src/assets/hero4.webp'
+      url: hero4
     }
   ]
   
